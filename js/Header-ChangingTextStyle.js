2bbd9ed30f9402da877b61949e6ee53b32ad2216
miskajurostudios <MiskaJuroStudios@gmail.com>
2024-10-29 08:13:56 +0100

    Update Header-ChangingTextStyle.js

--- js/Header-ChangingTextStyle.js
+++ js/Header-ChangingTextStyle.js
@@ -1,7 +1,9 @@
-const headers = ["𝖡𝗈𝗋𝖾𝖽", "𝓑𝓸𝓻𝓮𝓭", "𝙱𝚘𝚛𝚎𝚍", "𝔅𝔬𝔯𝔢𝔡"];
-let index = 0;
+if (typeof index === 'undefined') {
+    const headers = ["𝖡𝗈𝗋𝖾𝖽", "𝓑𝓸𝓻𝓮𝓭", "𝙱𝚘𝚛𝚎𝚍", "𝔅𝔬𝔯𝔢𝔡"];
+    let index = 0;
 
-setInterval(() => {
-    document.title = headers[index];
-    index = (index + 1) % headers.length;
-}, 500);
+    setInterval(() => {
+        document.title = headers[index];
+        index = (index + 1) % headers.length;
+    }, 500);
+}
