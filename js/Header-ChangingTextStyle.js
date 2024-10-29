e202155a650a8be798030482da1cc13386c2bd4a
miskajurostudios <MiskaJuroStudios@gmail.com>
2024-10-29 08:11:31 +0100

    Thicc

---
+++ js/Header-ChangingTextStyle.js
@@ -0,0 +1,7 @@
+const headers = ["𝖡𝗈𝗋𝖾𝖽", "𝓑𝓸𝓻𝓮𝓭", "𝙱𝚘𝚛𝚎𝚍", "𝔅𝔬𝔯𝔢𝔡"];
+let index = 0;
+
+setInterval(() => {
+    document.title = headers[index];
+    index = (index + 1) % headers.length;
+}, 500);
